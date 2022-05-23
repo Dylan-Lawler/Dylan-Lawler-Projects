d4f0a3c8914b27179a07891141fa98abe81d41af
Dylan G. Lawler <f004n2k@plank.thayer.dartmouth.edu>
2022-05-23 00:32:26 -0400

    05/22

--- sudoku.c
+++ sudoku.c
@@ -4,144 +4,107 @@
 #include <ctype.h>
 #include <time.h>
 #include "libcs50/counters.h"
+#include "sudokulib/board.h"
 
-typedef struct board {
-    counters_t **ptr_array;       // array of sets
-    int num_rows;              // number of rows in the hashboard
-} board_t;
-
-board_t *board_new(const int num_rows){
-    board_t *board = malloc(sizeof(board_t)); 
-
-    if (board == NULL){
-    return NULL;              // error allocating hashboard
-    } 
-    else{
-      board->num_rows = num_rows; 
-      board->ptr_array = calloc(board->num_rows, sizeof(counters_t*));
-      for (int i = 0; i < board->num_rows; i++){
-          board->ptr_array[i] = counters_new();
-          for (int j = 0; j < board->num_rows; j++){
-            counters_set(board->ptr_array[i], j, 0 );
-        }
-      }
-      return board;
-    }
-}
-
-int get_number(board_t *board, int row, int column){
-    return counters_get(board->ptr_array[row], column);
-}
-
-void insert_number(board_t *board, int row, int column, int number){
-    counters_set(board->ptr_array[row], column, number);
-}
-
+// local functions
 int *number_list();
-int *shuffle_list(int *num_list);
 bool full_board(board_t *board);
 bool check(board_t *board, int num, int row, int column);
-bool make_puzzle(board_t *board, int prevrow, int prevcol);
+bool fill_rest(board_t *board, int i, int j);
+bool make_puzzle(board_t *board, int row, int column);
 void print_board(board_t *board);
 void print_help(void *arg, const int key, const int count);
-
+void clear_spaces(board_t *board, int spaces);
 
 int main(int argc, char *argv[]){
+    srand(time(NULL));
     board_t *board = board_new(9);
     make_puzzle(board, 0, 0);
+    clear_spaces(board, 40);
     print_board(board);
 }
 
-bool make_puzzle(board_t *board, int prevrow, int prevcol){
-    int *numbers = number_list();
-    bool stop = true;
-    int row; 
-    int column;
-    for (row = 0; (row < 9) && (stop == true); row ++){
-        for (column = 0; column < 9; column++){
-            if ((get_number(board, row, column)) == 0){
-                for (int num = 0; num < 9; num++){
-                    srand(time(NULL));
-                    int *shuffled = shuffle_list(numbers);
-                    if ((check(board, shuffled[num], row, column)) == true){
-                        insert_number(board, row, column, shuffled[num]);
-                        if (full_board(board)){
-                            return true;
-                        }
-                        if ((make_puzzle(board, row, column)) == true){
-                            return true;
-                        }
-                    }
-
-                }
-                stop = false;
+// recursively make the puzzle by checking if each cell can hold any number 1-9
+bool make_puzzle(board_t *board, int row, int column){
+    // shuffled list of numbers 1-9
+    int *nums = number_list();
+    // if the board is full, make puzzle is done
+    if (full_board(board) == true){
+        return true;
+    }
+    // if its the end of the row, go to the next 
+    if (column >= 9 && row < 8){
+        row += 1; 
+        column = 0;
+    }
+    // for all possible number insertions
+    for (int num = 0; num<9; num++)
+    {
+        // if the number doesn't break sudoku rules
+        if (check(board, nums[num], row , column) == true)
+        {
+            // insert it
+            insert_number(board, row, column, nums[num]);
+            // go to the next number and do the same
+            if (make_puzzle(board, row, column+1)){
+                return true;
             }
+            // if no numbers work, keep deleting until numbers work again
+            insert_number(board, row, column, 0);
         }
     }
-    insert_number(board, prevrow, prevcol, 0);
+    // no numbers worked, backtrack
     return false;
 }
+
+void clear_spaces(board_t *board, int spaces){
+    // keeps track of spaces made empty
+    int cleared = 0; 
+    // until the desired amount of spaces are cleared
+    while(cleared < spaces){
+        // find a random cell
+        int row = rand() % 9;
+        int col = rand() % 9;
+        // if its not already emptied, empty it and increment number of empty cells
+        if (get_number(board, row, col) != 0){
+            insert_number(board, row, col, 0);
+            cleared++;
+        }
+    }
+}
+
 bool check(board_t *board, int num, int row, int column){
-    for (int i = 0; i < board->num_rows; i++){
-        if ((counters_get(board->ptr_array[row], i) == num) || (counters_get(board->ptr_array[i], column) == num)){
+    // if the number matches any number in the same row or column, return false
+    for (int i = 0; i < get_size(board); i++){
+        if ((counters_get(get_row(board, row), i) == num) || (counters_get(get_row(board, i), column) == num)){
             return false; 
         }
     }
+    // start at the top left corner of the square
     int boxrow = row - (row % 3);
     int boxcol = column - (column % 3);
 
+    // if the number matches any number in the square, return false 
     for (int i = boxrow; i < boxrow + 3; i ++){
         for (int j = boxcol; j < boxcol + 3; j ++){
-            if (counters_get(board->ptr_array[i], j) == num){
+            if (counters_get(get_row(board, i), j) == num){
                 return false;
             }
         }
     }
+    // the number is valid
     return true; 
 }
 
-bool full_board(board_t *board){
-    for (int i = 0; i < 9; i ++){
-        for (int j = 0; j < 9; j ++){
-            if (counters_get(board->ptr_array[i], j) == 0){
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-void print_board(board_t *board){
-    for (int i = 0; i < 9; i ++){
-        if (((i + 1) % 3 == 0) && i != 8){
-            counters_iterate(board->ptr_array[i], NULL, print_help);
-            printf("\n-------------------\n");
-
-        }
-        else{
-        counters_iterate(board->ptr_array[i], NULL, print_help);
-        printf("\n");
-        }
-    
-    }
-}
-
-void print_help(void *arg, const int key, const int count){
-    if ((key + 1) % 3 == 0 && key != 8){
-            printf("%d|", count);
-    }
-    else{
-            printf("%d ", count);
-    }
-}
 
 int *number_list(){
-    srand(time(NULL));
+    // create a list of possible numbers to insert to puzzle
     int *num_list = calloc(9, sizeof(int));
     for (int i = 0; i < 9; i++){
         num_list[i] = i + 1;
     }
 
+    // shuffle the numbers around so we can have a randomized puzzle
     for (int i = 0; i < 9; i++){
         int random = rand() % 9;
         int temp = num_list[i];
@@ -152,14 +115,3 @@ int *number_list(){
     return num_list;
 }
 
-int *shuffle_list(int *num_list){
-    srand(time(NULL));
-    for (int i = 0; i < 9; i++){
-        int random = rand() % 9;
-        int temp = num_list[i];
-        num_list[i] = num_list[random];
-        num_list[random] = temp;
-    }
-
-    return num_list;
-}
