dea06c6b0caf6d68c76103807c28c56e5ed6d551
Jordan J. Kirkbride <f003wpy@babylon1.thayer.dartmouth.edu>
2022-05-24 01:53:59 -0400

    added make.c make.h, updated Makefile

--- sudoku.c
+++ sudoku.c
@@ -5,12 +5,11 @@
 #include <time.h>
 #include "libcs50/counters.h"
 #include "sudokulib/board.h"
+#include "sudokulib/make.h"
 
 // local functions
-int *number_list();
 bool check(board_t *board, int num, int row, int column);
 bool fill_rest(board_t *board, int i, int j);
-bool make_puzzle(board_t *board, int row, int column);
 bool solve_puzzle(board_t *board, int row, int column);
 void clear_spaces(board_t *board, int spaces);
 
@@ -23,39 +22,6 @@ int main(int argc, char *argv[]){
     printf("\n");
     solve_puzzle(board, 0, 0);
     print_board(board);
-}
-
-// recursively make the puzzle by checking if each cell can hold any number 1-9
-bool make_puzzle(board_t *board, int row, int column){
-    // shuffled list of numbers 1-9
-    int *nums = number_list();
-    // if the board is full, make puzzle is done
-    if (full_board(board) == true){
-        return true;
-    }
-    // if its the end of the row, go to the next 
-    if (column >= 9 && row < 9){
-        row += 1; 
-        column = 0;
-    }
-    // for all possible number insertions
-    for (int num = 0; num<9; num++)
-    {
-        // if the number doesn't break sudoku rules
-        if (check(board, nums[num], row , column) == true)
-        {
-            // insert it
-            insert_number(board, row, column, nums[num]);
-            // go to the next number and do the same
-            if (make_puzzle(board, row, column+1)){
-                return true;
-            }
-            // if no numbers work, keep deleting until numbers work again
-            insert_number(board, row, column, 0);
-        }
-    }
-    // no numbers worked, backtrack
-    return false;
 }
 
 
@@ -96,6 +62,7 @@ bool solve_puzzle(board_t *board, int row, int column){
     return false;
 }
 
+
 void clear_spaces(board_t *board, int spaces){
     // keeps track of spaces made empty
     int cleared = 0; 
@@ -110,46 +77,4 @@ void clear_spaces(board_t *board, int spaces){
             cleared++;
         }
     }
-}
-
-bool check(board_t *board, int num, int row, int column){
-    // if the number matches any number in the same row or column, return false
-    for (int i = 0; i < get_size(board); i++){
-        if ((counters_get(get_row(board, row), i) == num) || (counters_get(get_row(board, i), column) == num)){
-            return false; 
-        }
-    }
-    // start at the top left corner of the square
-    int boxrow = row - (row % 3);
-    int boxcol = column - (column % 3);
-
-    // if the number matches any number in the square, return false 
-    for (int i = boxrow; i < boxrow + 3; i ++){
-        for (int j = boxcol; j < boxcol + 3; j ++){
-            if (counters_get(get_row(board, i), j) == num){
-                return false;
-            }
-        }
-    }
-    // the number is valid
-    return true; 
-}
-
-
-int *number_list(){
-    // create a list of possible numbers to insert to puzzle
-    int *num_list = calloc(9, sizeof(int));
-    for (int i = 0; i < 9; i++){
-        num_list[i] = i + 1;
-    }
-
-    // shuffle the numbers around so we can have a randomized puzzle
-    for (int i = 0; i < 9; i++){
-        int random = rand() % 9;
-        int temp = num_list[i];
-        num_list[i] = num_list[random];
-        num_list[random] = temp;
-    }
-
-    return num_list;
 }
