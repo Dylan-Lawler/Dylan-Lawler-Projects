7918d7d47830b78669741491071f0d812266998f
Alexander G. Jones <f0041jd@plank.thayer.dartmouth.edu>
2022-05-27 13:36:10 -0400

    Drafted solver func that checks for uniqueness

--- sudoku.c
+++ sudoku.c
@@ -16,7 +16,20 @@ int main(int argc, char *argv[]){
     clear_spaces(board, 40);
     print_board(board);
     printf("\n");
-    solve_puzzle(board, 0, 0);
+    int num_sols;
+    num_sols = solve_puzzle(board, 0, 0, 0);
+    if (num_sols < 1) {
+        fprintf(stderr, "Couldn't find a solution. Exiting.\n");
+        exit(1); // exit code 1: unsolvable board
+    }
+    if (num_sols == 1) {
+        printf("Found a unique solution: Here it is:\n");
+    }
+    else {
+        printf("Found at least 2 distinct solutions. Here's one of them:\n");
+    }
     print_board(board);
+
+    exit(0);
 }
 
--- sudokulib/solve.c
+++ sudokulib/solve.c
@@ -7,29 +7,30 @@
 #include "board.h"
 
 // recursively make the puzzle by checking if each cell can hold any number 1-9
-bool solve_puzzle(board_t *board, int row, int column){
-    // size of row/ column
+int solve_puzzle(board_t *board, int row, int column, int count){
+    // size of row/column
     int size = get_size(board);
-    // if the board is full, make puzzle is done, sets row and column to next empty cell otherwise
-    if (full_board(board, &row, &column) == true){
-        return true;
+    // If the board is full, then we're done with this solution
+    // Otherwise, set row and column to next empty cell
+    if (full_board(board, &row, &column)) {
+        // we found a solution, so increment count
+        printf("Found solution!\n");
+        print_board(board);
+        return count+1;
     }
 
-    for (int num = 1; num <= size; num++)
+    for (int num = 1; num <= size && count < 2; num++)
     {
         // if the number doesn't break sudoku rules
-        if ((check(board, num, row , column) == true))
+        if ((check(board, num, row , column)))
         {
             // insert it to the empty cell
             insert_number(board, row, column, num);
             // go to the next empty cell and do the same
-            if (solve_puzzle(board, row, column)){
-                return true;
-            }
-            // if no numbers work, keep deleting until numbers work again
-            insert_number(board, row, column, 0);
+            count = solve_puzzle(board, row, column, count);
         }
     }
     // no numbers worked, backtrack
-    return false;
+    insert_number(board, row, column, 0);
+    return count;
 }
