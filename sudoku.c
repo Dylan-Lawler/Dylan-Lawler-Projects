1be7135f40875a0d7555833a1464c0398e8bf1ba
Jordan J. Kirkbride <f003wpy@babylon1.thayer.dartmouth.edu>
2022-05-29 16:00:26 -0400

    valgrind issues fixed

--- sudoku.c
+++ sudoku.c
@@ -17,16 +17,20 @@ int main(int argc, char *argv[]){
     printf("\n");
     int num_sols;
     num_sols = solve_puzzle(board, 0, 0, 0);
+    
     if (num_sols < 1) {
         fprintf(stderr, "Couldn't find a solution. Exiting.\n");
-        exit(1); // exit code 1: unsolvable board
+        delete_puzzle(board);
+	exit(1); // exit code 1: unsolvable board
     }
+
     if (num_sols == 1) {
         printf("Found a unique solution: Here it is:\n");
     }
     else {
         printf("Found at least 2 distinct solutions. Here's one of them:\n");
     }
+
     print_solution(board);
     delete_puzzle(board);
 
