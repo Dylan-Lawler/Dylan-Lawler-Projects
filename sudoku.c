6090a6fb7b2233171bf0a14dce05aa26c85a75a4
Dylan G. Lawler <f004n2k@plank.thayer.dartmouth.edu>
2022-05-29 16:35:59 -0400

    faster unique

--- sudoku.c
+++ sudoku.c
@@ -1,3 +1,4 @@
+#pragma GCC target("avx2") 
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
@@ -12,23 +13,24 @@ int main(int argc, char *argv[]){
     srand(time(NULL));
     board_t *board = board_new(9);
     make_puzzle(board, 0, 0);
-    clear_spaces(board, 40);
+    clear_spaces(board, 5);
     print_board(board);
     printf("\n");
-    int num_sols;
-    num_sols = solve_puzzle(board, 0, 0, 0);
-    if (num_sols < 1) {
-        fprintf(stderr, "Couldn't find a solution. Exiting.\n");
-        exit(1); // exit code 1: unsolvable board
-    }
-    if (num_sols == 1) {
-        printf("Found a unique solution: Here it is:\n");
-    }
-    else {
-        printf("Found at least 2 distinct solutions. Here's one of them:\n");
-    }
-    print_solution(board);
-    delete_puzzle(board);
+    
+    // int num_sols;
+    // num_sols = solve_puzzle(board, 0, 0, 0);
+    // if (num_sols < 1) {
+    //     fprintf(stderr, "Couldn't find a solution. Exiting.\n");
+    //     exit(1); // exit code 1: unsolvable board
+    // }
+    // if (num_sols == 1) {
+    //     printf("Found a unique solution: Here it is:\n");
+    // }
+    // else {
+    //     printf("Found at least 2 distinct solutions. Here's one of them:\n");
+    // }
+    // print_solution(board);
+    // delete_puzzle(board);
 
-    exit(0);
+    // exit(0);
 }
